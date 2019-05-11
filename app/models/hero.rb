452421d0d96d0a2c8e46bf2c9124ab5e17c24174
Lucas Manzano Farias <lucasmfarias1@gmail.com>
2019-05-11 17:32:55 -0300

    tweak female chainmail image; fix menu-button positioning; fix hero model indentation and spacing

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,6 +1,8 @@
 class Hero < ApplicationRecord
-  has_one :hero_gear, class_name: :HeroGear, foreign_key: :hero_id,
-    dependent: :destroy
+  has_one :hero_gear,
+          class_name: :HeroGear,
+          foreign_key: :hero_id,
+          dependent: :destroy
 
   def gear_id
     if hero_gear
